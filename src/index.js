56d9e7b3cef5fc1139f9a1b4c7e85f51ca4fb76d
Muhammad Abrar <abrarmuhammad100@gmail.com>
2021-12-16 11:14:03 +0500

    cooltipz clear

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'carbon-components/css/carbon-components.min.css';
 // import './cooltipz.css';
-import 'cooltipz-css';
+// import 'cooltipz-css';
 import './index.css';
 import App from './App';
 import { store } from './store/store'
